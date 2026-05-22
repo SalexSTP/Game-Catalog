c78d679b9e20e0d1e437f5bc0450fe0669fc206b
Aleksandar-Stoyanov <135261672+SalexSTP@users.noreply.github.com>
2026-05-22 19:33:14 +0300

    Migration for Game Id Changes

---
+++ GameCatalog/Data/Migrations/20260522163206_GameId.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class GameId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Games",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValueSql: "NEWSEQUENTIALID()",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Games",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldDefaultValueSql: "NEWSEQUENTIALID()");
+        }
+    }
+}
